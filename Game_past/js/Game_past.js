5905e3658a3a92a073b40971bcb980fd0764b122
tootoo2201 <122020878+tootoo2201@users.noreply.github.com>
2023-08-03 16:03:57 +0900

    past

--- Game_past/js/Game_past.js
+++ Game_past/js/Game_past.js
@@ -2,10 +2,11 @@ let container = document.querySelector("#container");
 let character = document.querySelector("#character");
 let crystalball = document.querySelector("#crystalball");
 let block = document.querySelector("#block");
+let monster = document.querySelector("#monster");
 let road = document.querySelector("#road");
 let score = document.querySelector("#score");
 let gameOver = document.querySelector("#gameOver");
-let crystalballAppearances = 0; // count crystalball appearances
+let crystalballCount = 0; // Count crystalball appearances
 
 // Life elements
 let life1 = document.querySelector("#life1");
@@ -18,12 +19,6 @@ let lives = [life1, life2, life3];  // array to keep track of lives
 let interval = null;
 let playerScore = 0;
 
-// // function for score
-// let scoreCounter = () => {
-//     playerScore++;
-//     score.innerHTML = `Score <b>${playerScore}</b>`;
-// }
-
 // Collision state
 let collisionState = false;
 
@@ -32,7 +27,8 @@ let gameRunning = false;
 
 // start Game
 window.addEventListener("keydown", (start) => {
-    if (start.code == "Space" && !gameRunning) {  // Check if the game is not running
+    if (start.code == "Space" && !gameRunning && lives.length > 0) {  // Check if the game is not running
+        crystalballCount = 0;
         if (lives.length <= 0) {
             // reset lives
             lives = [life1, life2, life3];  // Reset the lives
@@ -41,14 +37,14 @@ window.addEventListener("keydown", (start) => {
         road.firstElementChild.style.animation = "roadAnimate 2.5s linear infinite";
         gameOver.style.display = "none";
         block.classList.add("blockActive");
-        crystalball.classList.add("crystalballActive");  
+        monster.classList.add("monsterActive");
+        crystalball.classList.add("crystalballActive");
 
         // score
-        playerScore = 0.00;
-        score.innerHTML = `Score <b>${playerScore.toFixed(2)}</b>`;
+        playerScore = 0;
+        score.innerHTML = `Score <b>${playerScore}</b>`;
         collisionState = false;
         gameRunning = true;
-        crystalBallsRemaining = 15;
         //interval = setInterval(scoreCounter, 200);
     }
 });
@@ -65,11 +61,84 @@ window.addEventListener("keydown", (e) => {
         }
 });
 
+let isCounted = false; // new flag
+
+// Flag to check if the score was already added
+let scoreAdded = false;
+
+// Check game status
+function checkGameStatus() {
+    if (crystalballCount >= 5) {  // When all 10 crystalballs have appeared
+        block.classList.remove("blockActive");
+        crystalball.classList.remove("crystalballActive");  // Stop the crystalball
+        road.firstElementChild.style.animation = "none";
+        clearInterval(result);
+        gameRunning = false;
+        if (playerScore >= 5) {  // If score is 10 or more
+            gameOver.innerHTML = "Game clear!";
+            gameOver.style.display = "block";
+
+        } else {  // If score is less than 10
+            gameOver.innerHTML = "Stage success! Collect more crystalball";
+            gameOver.style.display = "block";
+        }
+        // Reset block, crystalball and character animations
+        block.style.animation = "";
+        crystalball.style.animation = "";
+        character.style.animation = "";
+
+        // Set the game over flag
+        gameRunning = true;
+    }
+}
+function pauseGame() {
+    road.firstElementChild.style.animation = "none"; // 도로 애니메이션 중지
+    block.classList.remove("blockActive"); // 블록 애니메이션 중지
+    crystalball.classList.remove("crystalballActive"); // 크리스탈볼 애니메이션 중지
+    clearInterval(result); // 게임 로직 중지
+    gameRunning = false; // 게임 상태를 중지로 설정
+}
+
+function showExclamationAndRedirect() {
+    pauseGame(); // 게임을 일시 중지
+    let exclamation = document.querySelector("#exclamation");
+    exclamation.style.display = "block"; // 느낌표 보이기
+    setTimeout(() => {
+        exclamation.style.display = "none"; // 느낌표 숨기기
+        window.location.href = "new_page.html"; // 새 페이지로 이동
+    }, 3000); // 3초 후 실행
+}
+
+
 //'Game Over' if 'Character' hit The 'Block' 
 let result = setInterval(() => {
     let characterBottom = parseInt(getComputedStyle(character).getPropertyValue("bottom"));
+    let characterTop = parseInt(getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(getComputedStyle(block).getPropertyValue("left"));
     let crystalballLeft = parseInt(getComputedStyle(crystalball).getPropertyValue("left"));
+    let monsterLeft = parseInt(getComputedStyle(monster).getPropertyValue("left")); // monster의 left 위치
+
+    // Increase the crystalball count when it starts to appear from the right side
+    if (crystalballLeft <= 0 && !isCounted && crystalballCount < 5) {
+        crystalballCount++;
+        console.log(crystalballCount);
+        isCounted = true;
+    }
+    // 게임 중 느낌표를 보여줄 조건이 충족되면 위 함수를 호출
+    // 예: 블록과 캐릭터가 충돌하는 경우
+    if (monsterLeft < 50 && monsterLeft > 20 && characterBottom < 90) {
+        showExclamationAndRedirect();
+    }
+
+    // Reset the flag when the next crystalball appears
+    if (crystalballLeft > 0) {
+        isCounted = false;
+    }
+
+    // Stop the crystalball when the count reaches 10
+    if (crystalballCount >= 5) {
+        checkGameStatus();  // Check the game status
+    }
 
     if (!collisionState && characterBottom <= 90 && blockLeft >= 20 && blockLeft <= 50) {
         // add 'bounce' and 'redFlash' classes to character
@@ -87,10 +156,22 @@ let result = setInterval(() => {
         if (lives.length <= 0) {  // When no more lives left
             gameOver.style.display = "block";
             block.classList.remove("blockActive");
+            crystalball.classList.remove("crystalballActive");  // Stop the crystalball
             road.firstElementChild.style.animation = "none";
             clearInterval(interval);
-            playerScore = 0.00;
+            playerScore = 0;
             gameRunning = false;
         }
+    }
+
+    // When character and crystalball collide, increase the score by 1
+    if (characterTop <= 250 && crystalballLeft >= 80 && crystalballLeft <= 100) {
+        if (!scoreAdded) {
+            playerScore += 1; // increment score by 1
+            score.innerHTML = `Score <b>${playerScore}</b>`; // Update the score on the screen
+            scoreAdded = true;
+        }
+    } else {
+        scoreAdded = false; // Reset the score added flag when not colliding
     }
 }, 10);
