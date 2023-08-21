cf2e5bf856ca0fbd29593924b7f5c6aa36aa417b
tootoo2201 <122020878+tootoo2201@users.noreply.github.com>
2023-08-21 14:40:20 +0900

    몬스터 등장 안하게

--- Game_past_2/js/Game_past_2.js
+++ Game_past_2/js/Game_past_2.js
@@ -336,6 +336,7 @@ async function checkGameStatus() {
     if (scoreFromDB >= 5) {  // 데이터베이스로부터 가져온 점수가 5 이상일 때
         block.classList.remove("blockActive");
         crystalball.classList.remove("crystalballActive");
+        monster.classList.remove("monsterActive");  // 몬스터 애니메이션 일시 중지
         road.firstElementChild.style.animation = "none";
         clearInterval(result);
         gameRunning = false;
@@ -359,6 +360,7 @@ function pauseGame() {
     road.firstElementChild.style.animation = "none"; // 도로 애니메이션 중지
     block.classList.remove("blockActive"); // 블록 애니메이션 중지
     crystalball.classList.remove("crystalballActive"); // 크리스탈볼 애니메이션 중지
+    monster.classList.remove("monsterActive");  // 몬스터 애니메이션 일시 중지
     clearInterval(result); // 게임 로직 중지
     gameRunning = false; // 게임 상태를 중지로 설정
 }
@@ -533,6 +535,7 @@ let result = setInterval(async () => {
                 gameOver.style.display = "block";
                 block.classList.remove("blockActive");
                 crystalball.classList.remove("crystalballActive");
+                monster.classList.remove("monster");
                 road.firstElementChild.style.animation = "none";
                 clearInterval(interval);
                 playerScore = 0;
